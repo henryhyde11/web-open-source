95c09de69e36e0e335828ed52052a8478c0c6213
henryhyde <serestrepov@gmail.com>
2023-03-16 00:15:19 -0500

    Body selector deleated

--- script/main.js
+++ script/main.js
@@ -1,7 +1,5 @@
 const navLeftImg = document.querySelector('.nav-left__img');
 const menuDropLeft = document.querySelector('.menu-drop-down-left');
-const body = document.querySelector('.body');
-
 
 navLeftImg.addEventListener('click', ()=>{
     if (menuDropLeft.classList.contains('hide')){
