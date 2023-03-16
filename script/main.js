df6bdabae4d4a999137d538835142fd158018f5e
henryhyde <serestrepov@gmail.com>
2023-03-16 12:50:12 -0500

    Body selector deleted

--- script/main.js
+++ script/main.js
@@ -1,10 +1,10 @@
 const navLeftImg = document.querySelector('.nav-left__img');
 const menuDropLeft = document.querySelector('.menu-drop-down-left');
 
-navLeftImg.addEventListener('click', ()=>{
-    if (menuDropLeft.classList.contains('hide')){
+navLeftImg.addEventListener('click', () => {
+    if (menuDropLeft.classList.contains('hide')) {
         menuDropLeft.classList.remove('hide');
-    }else{
+    } else {
         menuDropLeft.classList.add('hide');
     }
 })
